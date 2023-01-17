7ef18661d898d27da561f723f59a8a5ff5ac17ea
Hack Kiếm Sĩ <84692657+phanxuanquang@users.noreply.github.com>
2023-01-17 20:54:52 +0700

    Update 04.cpp

--- BTH1/04.cpp
+++ BTH1/04.cpp
@@ -1,77 +1,103 @@
 #include <iostream>
 using namespace std;
-class date {
+
+bool isLeapYear(int year)
+{
+	if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0))
+		return true;
+	return false;
+}
+
+class Date {
+	int day, month, year;
+
 public:
-	int day;
-	int month;
-	long year;
+	Date() {}
+	Date(int day, int month, int year) {
+		this->day = day;
+		this->month = month;
+		this->year = year;
+	}
+
+	bool isValidDate() {
+		if (year < 0)
+			return false;
+		if (month < 1 || month > 12)
+			return false;
+		if ((month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12) && (day > 31))
+			return false;
+		if ((month == 4 || month == 6 || month == 9 || month == 11) && (day > 30))
+			return false;
+		if (isLeapYear(year) && month == 2 && day > 29)
+			return false;
+		return true;
+	}
+	Date getNextDate()
+	{
+		Date nextDate(this->day, this->month, this->year);
+		nextDate.day++;
+		
+		if (nextDate.month == 1 || nextDate.month == 3 || nextDate.month == 5 || nextDate.month == 7 || nextDate.month == 8 || nextDate.month == 10 || nextDate.month == 12)
+		{
+			if (nextDate.day >= 31 && nextDate.month != 12)
+			{
+				nextDate.day = 1;
+				nextDate.month++;
+			}
+			else if (nextDate.day >= 31 && nextDate.month == 12) {
+				nextDate.day = 1;
+				nextDate.month = 1;
+				nextDate.year++;
+			}
+		}
+
+		if ((nextDate.month == 4 || nextDate.month == 6 || nextDate.month == 9 || nextDate.month == 11))
+		{
+			if (day >= 30)
+			{
+				day = 1;
+				month++;
+			}
+		}
+
+		if (isLeapYear(nextDate.year) && nextDate.month == 2) {
+			if (nextDate.day >= 29)
+			{
+				nextDate.day = 1;
+				nextDate.month++;
+			}
+		}
+
+		return nextDate;
+	}
+
+	void Import() {
+		cout << "  Nhap ngay: ";
+		cin >> day;
+		cout << "  Nhap thang: ";
+		cin >> month;
+		cout << "  Nhap nam: ";
+		cin >> year;
+		if (!isValidDate()) {
+			cout << "Ngay thang nam khong hop le, vui long nhap lai" << endl;
+			Import();
+		}
+	}
+	void Export() {
+		cout << day << "/" << month << "/" << year << endl;
+	}
 };
-void import(date& x, int& Month_LastDate);
-void month_case(date& x, int& Month_LastDate);
-void nextDate(date x, date& y, int n);
-void main() {
-	date date1, date2;
-	int Month_LastDate;
-	import(date1, Month_LastDate);
-	month_case(date1, Month_LastDate);
-	nextDate(date1, date2, Month_LastDate);
-	cout << "Ngay tiep theo la " << date2.day << "/" << date2.month << "/" << date2.year << endl;
+
+int main()
+{
+	Date date;
+	cout << "Nhap ngay thang nam: " << endl;
+	date.Import();
+
+	system("cls");
+
+	cout << "Ngay tiep theo la ";
+	date.getNextDate().Export();
+
 	system("pause");
-}
-void import(date& x, int& Month_LastDate)
-{
-	cout << " Nhap ngay: ";
-	cin >> x.day;
-	cout << " Nhap thang: ";
-	cin >> x.month;
-	cout << " Nhap nam: ";
-	cin >> x.year;
-}
-void month_case(date& x, int& Month_LastDate) {
-	switch (x.month)
-	{
-	case 1: case 3: case 5: case 7: case 8: case 10: case 12:
-		Month_LastDate = 31;
-		break;
-	case 4: case 6: case 9: case 11:
-		Month_LastDate = 30;
-		break;
-	case 2:
-		if (x.year % 400 == 0 || (x.year % 4 == 0 && x.year % 100 != 0))
-			Month_LastDate = 29;
-		else Month_LastDate = 28;
-		break;
-	default: 
-		Month_LastDate = 0;
-		break;
-	}
-	while (Month_LastDate == 0 || x.year < 1 || (Month_LastDate != 0 && (x.day > Month_LastDate || x.day < 1)))
-	{
-		cout << endl << "Ngay thanh nam khong hop le, vui long nhap lai." << endl;
-		import(x, Month_LastDate);
-	}
-}
-void nextDate(date x, date& y, int n)
-{
-	if (x.day < n)
-	{
-		y.day = x.day + 1;
-		y.month = x.month;
-		y.year = x.year;
-	}
-	else
-	{
-		if (x.month != 12)
-		{
-			y.day = 1;
-			y.month = x.month + 1;
-			y.year = x.year;
-		}
-		else
-		{
-			y.day = 1;
-			y.month = 1;
-			y.year = x.year + 1;
-		}
-	}
 }
