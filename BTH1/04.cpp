0f9d9e5cb01444508d52cdf4b2d857452cc93c8d
Hack Kiếm Sĩ <84692657+phanxuanquang@users.noreply.github.com>
2023-01-23 20:22:48 +0700

    Update 04.cpp

--- BTH1/04.cpp
+++ BTH1/04.cpp
@@ -12,36 +12,51 @@ public:
 		this->year = year;
 	}
 
+	void input();
+	void output();
+
+	bool isLeapYear();
 	bool isValidDate();
+	
 	Date getNextDate();
-
-	void Import();
-	void Export();
 };
-
-bool isLeapYear(int year);
 
 int main()
 {
 	Date date;
 	cout << "Nhap ngay thang nam: " << endl;
-	date.Import();
-
-	system("cls");
+	date.input();
 
 	cout << "Ngay tiep theo la ";
-	date.getNextDate().Export();
+	date.getNextDate().output();
 
 	system("pause");
 }
 
-bool isLeapYear(int year)
+void Date::input() {
+	cout << "  Nhap ngay: ";
+	cin >> day;
+	cout << "  Nhap thang: ";
+	cin >> month;
+	cout << "  Nhap nam: ";
+	cin >> year;
+
+	if (!isValidDate()) {
+		system("cls");
+		cout << "Ngay thang nam khong hop le, vui long nhap lai" << endl;
+		input();
+	}
+}
+void Date::output() {
+	cout << day << "/" << month << "/" << year << endl;
+}
+
+bool Date::isLeapYear()
 {
 	if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0))
 		return true;
 	return false;
 }
-
 bool Date::isValidDate() {
 	if (year < 0)
 		return false;
@@ -51,67 +66,45 @@ bool Date::isValidDate() {
 		return false;
 	if ((month == 4 || month == 6 || month == 9 || month == 11) && (day > 30))
 		return false;
-	if ((isLeapYear(year) && month == 2 && day > 29) || (!isLeapYear(year) && month == 2 && day > 28))
+	if ((isLeapYear() && month == 2 && day > 29) || (!isLeapYear() && month == 2 && day > 28))
 		return false;
 	return true;
 }
-Date Date::getNextDate()
-{
-	Date nextDate(this->day, this->month, this->year);
-	nextDate.day++;
 
-	if (nextDate.month == 1 || nextDate.month == 3 || nextDate.month == 5 || nextDate.month == 7 || nextDate.month == 8 || nextDate.month == 10 || nextDate.month == 12)
-	{
-		if (nextDate.day >= 31 && nextDate.month != 12)
-		{
-			nextDate.day = 1;
-			nextDate.month++;
-		}
-		else if (nextDate.day >= 31 && nextDate.month == 12) {
-			nextDate.day = 1;
-			nextDate.month = 1;
-			nextDate.year++;
+Date Date::getNextDate() {
+	Date tempDate(++this->day, this->month, this->year);
+
+	if (tempDate.day > 31) {
+		tempDate.day = 1;
+		tempDate.month++;
+		if (tempDate.month > 12) {
+			tempDate.month = 1;
+			tempDate.year++;
 		}
 	}
-
-	if ((nextDate.month == 4 || nextDate.month == 6 || nextDate.month == 9 || nextDate.month == 11))
-	{
-		if (day > 30)
+	else if (tempDate.day > 30) {
+		if ((tempDate.month == 4 || tempDate.month == 6 || tempDate.month == 9 || tempDate.month == 11))
 		{
-			day = 1;
-			month++;
+			tempDate.day = 1;
+			tempDate.month++;
 		}
 	}
-
-	if (isLeapYear(nextDate.year) && nextDate.month == 2) {
-		if (nextDate.day > 29)
-		{
-			nextDate.day = 1;
-			nextDate.month++;
+	else if (tempDate.month == 2) {
+		if (isLeapYear()) {
+			if (tempDate.day > 29)
+			{
+				tempDate.day = 1;
+				tempDate.month++;
+			}
 		}
-	}
-	if (!isLeapYear(nextDate.year) && nextDate.month == 2) {
-		if (nextDate.day > 28)
-		{
-			nextDate.day = 1;
-			nextDate.month++;
+		else {
+			if (tempDate.day > 28)
+			{
+				tempDate.day = 1;
+				tempDate.month++;
+			}
 		}
 	}
 
-	return nextDate;
-}
-void Date::Import() {
-	cout << "  Nhap ngay: ";
-	cin >> day;
-	cout << "  Nhap thang: ";
-	cin >> month;
-	cout << "  Nhap nam: ";
-	cin >> year;
-	if (!isValidDate()) {
-		cout << "Ngay thang nam khong hop le, vui long nhap lai" << endl;
-		Import();
-	}
-}
-void Date::Export() {
-	cout << day << "/" << month << "/" << year << endl;
+	return tempDate;
 }
