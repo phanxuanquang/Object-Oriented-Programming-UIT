57473ba3a476eceece95e0d6c4037fa9bd3fde2d
Hack Kiếm Sĩ <84692657+phanxuanquang@users.noreply.github.com>
2023-01-23 21:06:24 +0700

    Update 01.cpp

--- BTH3/01.cpp
+++ BTH3/01.cpp
@@ -1,113 +1,139 @@
 #include <iostream>
 using namespace std;
 
-class point {
+class Point {
     double x, y;
+
 public:
-    void change(double, double);
-    double get_x();
-    double get_y();
-    void point_import();
-    void point_export();
-    void move_vector();
+    Point() {
+        x = y = 0;
+    }
+
+    double getX();
+    double getY();
+
+    void set(double, double);
+
+    void input();
+    void output();
 };
 
-void call_menu();
-void choice(point&);
+void printMenu();
+void choose(Point&);
+
+void printPointX(Point);
+void printPointY(Point);
+void printPoint(Point);
+void changePoint(Point&);
+void move_vector(Point&);
 
 int main() {
-    point x;
+    Point point;
     cout << "Nhap toa do cua diem: ";
-    x.point_import();
-    call_menu();
-    choice(x);
+    point.input();
+
+    choose(point);
+
     system("pause");
 }
 
-void call_menu() {
-    cout << "_______________________________" << endl;
-    cout << "1. Lay hoanh do" << endl;
-    cout << "2. Lay tung do" << endl;
-    cout << "3. Tinh tien diem" << endl;
-    cout << "4. Thay doi toa do diem" << endl;
-    cout << "5. Xuat toa do diem" << endl;
-    cout << "_______________________________" << endl;
-}
-void choice(point& a) {
-    int x;
-    cout << "Nhap lua chon: ";
-    cin >> x;
-    switch (x) {
-        case 1:
-            system("cls");
-            cout << "Hoanh do cua diem la " << a.get_x() << endl;
-            call_menu();
-            choice(a);
-            break;
-        case 2:
-            system("cls");
-            cout << "Tung do cua diem la " << a.get_y() << endl;
-            call_menu();
-            choice(a);
-            break;
-        case 3:
-            system("cls");
-            a.move_vector();
-            call_menu();
-            choice(a);
-            break;
-        case 4:
-            system("cls");
-            point new_point;
-            cout << "Nhap toa do diem moi" << endl;
-            new_point.point_import();
-            a.change(new_point.get_x(), new_point.get_y());
-            cout << "Thay doi toa do diem thanh cong";
-            call_menu();
-            choice(a);
-            break;
-        case 5:
-            system("cls");
-            cout << "Diem co toa do ";
-            a.point_export();
-            call_menu();
-            choice(a);
-            break;
-        default:
-            system("cls");
-            cout << "Lua chon khong ton tai" << endl;
-            call_menu();
-            choice(a);
-            break;
-    }
-}
-
-double point::get_x() {
+double Point::getX() {
     return x;
 }
-double point::get_y() {
+double Point::getY() {
     return y;
 }
-void point::change(double x, double y) {
+void Point::set(double x, double y) {
     this->x = x;
     this->y = y;
 }
-void point::point_import() {
+void Point::input() {
     cout << "\n Hoanh do: ";
     cin >> x;
     cout << " Tung do: ";
     cin >> y;
 }
-void point::point_export() {
+void Point::output() {
     cout << "(" << x << "," << y << ")" << endl;
 }
-void point::move_vector() {
-    double x, y;
-    cout << "Hoanh do cua vector tinh tien: ";
-    cin >> x;
-    cout << "Tung do cua vector tinh tien: ";
-    cin >> y;
+
+void printPointX(Point point) {
+    cout << "Hoanh do cua diem la " << point.getX() << endl;
+}
+void printPointY(Point point) {
+    cout << "Hoanh do cua diem la " << point.getY() << endl;
+}
+void printPoint(Point point) {
+    cout << "Toa do cua diem la ";
+    point.output();
+}
+void changePoint(Point &point) {
+    int newX, newY;
+    cout << "Hoang do moi: ";
+    cin >> newX;
+    cout << "Tung do moi: ";
+    cin >> newY;
     system("cls");
-    change(x + this->x, y + this->y);
-    cout << "Tinh tien diem thanh cong" << endl;
+
+    point.set(newX, newY);
+
+    cout << "Thay doi toa do diem thanh cong. Toa do moi la ";
+    point.output();
+}
+void move_vector(Point& point) {
+    double vectorX, vectorY;
+    cout << "Hoanh do cua vector tinh tien: ";
+    cin >> vectorX;
+    cout << "Tung do cua vector tinh tien: ";
+    cin >> vectorY;
+    system("cls");
+
+    point.set(vectorX + point.getX(), vectorY + point.getY());
+
+    cout << "Tinh tien diem thanh cong. Toa do moi la ";
+    point.output();
+}
+
+void printMenu() {
+    cout << "_______________________________" << endl;
+    cout << " 1. Lay hoanh do" << endl;
+    cout << " 2. Lay tung do" << endl;
+    cout << " 3. Tinh tien diem" << endl;
+    cout << " 4. Thay doi toa do diem" << endl;
+    cout << " 5. Xuat toa do diem" << endl;
+    cout << " 6. Thoat" << endl;
+    cout << "_______________________________" << endl;
+}
+void choose(Point& point) {
+    printMenu();
+
+    int choice;
+    cout << "Lua chon cua ban la: ";
+    cin >> choice;
+    system("cls");
+
+    switch (choice) {
+    case 1:
+        printPointX(point);
+        break;
+    case 2:
+        printPointY(point);
+        break;
+    case 3:
+        move_vector(point);
+        break;
+    case 4:
+        changePoint(point);
+        break;
+    case 5:
+        printPoint(point);
+        break;
+    case 6:
+        return;
+    default:
+        cout << "Lua chon khong ton tai" << endl;
+        break;
+    }
+    
+    choose(point);
 }
