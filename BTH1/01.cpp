456828c3654069e1a8fef73302ce2c2e72f62d04
Hack Kiếm Sĩ <84692657+phanxuanquang@users.noreply.github.com>
2023-01-23 20:22:23 +0700

    Update 01.cpp

--- BTH1/01.cpp
+++ BTH1/01.cpp
@@ -1,48 +1,54 @@
 #include <iostream>
 using namespace std;
-class fraction {
+
+class Fraction {
 	int numerator, denominator;
+
 public:
-	int get_numerator() {
+	int getNumerator() {
 		return this->numerator;
 	}
-	int get_denominator() {
+	int getDenominator() {
 		return this->denominator;
 	}
-	void import();
-	void print();
+
 	void simplify();
-	~fraction() {
-	}
+
+	void input();
+	void output();
 };
-void main()
+
+int main()
 {
-	fraction *a = new fraction;
-	a->import();
-	cout << "Phan so sau khi rut gon la: ";
-	a->print();
-	delete a;
+	Fraction a;
+	a.input();
+
+	cout << "Ket qua rut gon la ";
+	a.output();
+
 	system("pause");
 }
-void fraction::import() {
-	cout << " Tu so la: ";
+
+void Fraction::input() {
+	cout << "Tu so la: ";
 	cin >> numerator;
-	cout << " Mau so la: ";
+	cout << "Mau so la: ";
 	cin >> denominator;
 	while (denominator == 0) {
-		cout << "Mau so phai khac 0. Mau so moi la: ";
+		cout << "  Mau so phai khac 0. Mau so moi la: ";
 		cin >> denominator;
 	}
 	cout << endl;
 }
-void fraction::print() {
+void Fraction::output() {
 	simplify();
 	if (numerator % denominator == 0)
 		cout << numerator / denominator;
 	else cout << numerator << "/" << denominator;
 	cout << endl;
 }
-void fraction::simplify() {
+
+void Fraction::simplify() {
 	for (int i = abs(numerator); i >= 1; i--)
 		if (numerator % i == 0 && denominator % i == 0) {
 			numerator /= i;
