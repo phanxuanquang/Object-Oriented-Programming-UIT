542340802fa92d05ad908ae86cff5dc9c77197e1
Hack Kiếm Sĩ <84692657+phanxuanquang@users.noreply.github.com>
2023-03-01 00:46:15 +0700

    Use ChatGPT to optimize code

--- BTH4/03.cpp
+++ BTH4/03.cpp
@@ -1,29 +1,32 @@
 #include <iostream>
+
 using namespace std;
 
-class stack {
-    int current_size = -1;
+class Stack {
+    int currentSize = -1;
     int* arr = new int[1000];
+
 public:
-    void stack_export(int x);
+    void output(int x);
     void push(int);
 };
 
 bool isPrime(int);
-void add_toStack(int, int, stack&);
+void addToStack(int, int, Stack&);
 
-int main()
-{
-    int x;
-    stack Stack;
+int main() {
+    int number;
+    Stack stack;
     cout << "So can phan tich la ";
-    cin >> x;
-    for (int i = 2; i < x - 1; i++) {
-        if (x % i == 0 && isPrime(i))
-            add_toStack(x, i, Stack);
+    cin >> number;
+
+    for (int i = 2; i < number - 1; i++) {
+        if (number % i == 0 && isPrime(i))
+            addToStack(number, i, stack);
     }
     system("cls");
-    Stack.stack_export(x);
+
+    stack.output(number);
     system("pause");
 }
 
@@ -35,20 +38,22 @@ bool isPrime(int x) {
             return false;
     return true;
 }
-void add_toStack(int number, int index, stack& Stack) {
+
+void addToStack(int number, int index, Stack& stack) {
     while (number % index == 0) {
-        Stack.push(index);
+        stack.push(index);
         number /= index;
     }
 }
 
-void stack::stack_export(int x) {
+void Stack::output(int x) {
     cout << x << " = ";
-    for (int i = current_size; i > 0; i--)
+    for (int i = currentSize; i > 0; i--)
         cout << arr[i] << " * ";
     cout << arr[0] << endl;
 }
-void stack::push(int add_element) {
-        current_size++;
-        arr[current_size] = add_element;
+
+void Stack::push(int addElement) {
+    currentSize++;
+    arr[currentSize] = addElement;
 }
