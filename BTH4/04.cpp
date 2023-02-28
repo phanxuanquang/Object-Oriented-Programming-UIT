fc9fc0355fd5b0870bc24d1a9cbed93a7fffe758
Hack Kiếm Sĩ <84692657+phanxuanquang@users.noreply.github.com>
2023-03-01 00:59:04 +0700

    Use ChatGPT to optimize code

--- BTH4/04.cpp
+++ BTH4/04.cpp
@@ -1,120 +1,128 @@
 #include <iostream>
 using namespace std;
 
-class stack {
-    int current_size = -1;
+class Stack {
+    int currentSize = -1;
     int* arr = new int[1000];
+
 public:
-    void stack_export(int x);
+    void stackExport(int x);
     void push(int);
 };
-void to_binary(int number);
-void to_octal(int number);
-void to_hexa(int number);
 
-void call_menu() {
+void toBinary(int number);
+void toOctal(int number);
+void toHexa(int number);
+
+void callMenu();
+
+void choose(int);
+
+int main() {
+    int number;
+    Stack stack;
+    cout << "So can phan tich la ";
+    cin >> number;
+
+    choose(number);
+}
+
+void callMenu() {
     cout << "________________________________" << endl;
-    cout << "1. Doi sang he nhi phan" << endl;
-    cout << "2. Doi sang he bat phan" << endl;
-    cout << "3. Doi sang he thap luc phan" << endl;
+    cout << " (0) Thoat chuong trinh." << endl;
+    cout << " (1) Doi sang he nhi phan." << endl;
+    cout << " (2) Doi sang he bat phan." << endl;
+    cout << " (3) Doi sang he thap luc phan." << endl;
     cout << "________________________________" << endl;
 }
-void choose(int x) {
+void choose(int number) {
+    callMenu();
+
     int choice;
     cout << "Lua chon cua ban la: ";
     cin >> choice;
-    switch (choice){
+
+    system("cls");
+    switch (choice) {
+    case 0:
+        return;
     case 1:
-        system("cls");
-        to_binary(x);
-        call_menu();
-        choose(x);
+        toBinary(number);
         break;
     case 2:
-        system("cls");
-        to_octal(x);
-        call_menu();
-        choose(x);
+        toOctal(number);
         break;
     case 3:
-        system("cls");
-        to_hexa(x);
-        call_menu();
-        choose(x);
+        toHexa(number);
         break;
     default:
-        system("cls");
         cout << "Lua chon khong hop le, hay nhap lai" << endl;
-        call_menu();
-        choose(x);
         break;
     }
+    choose(number);
 }
 
-int main(){
-    int x;
-    stack Stack;
-    cout << "So can phan tich la ";
-    cin >> x;
-    call_menu();
-    choose(x);
-}
-
-void to_binary(int number) {
-    if (number == 0)
+void toBinary(int number) {
+    if (number == 0) {
         cout << number << " = " << 0;
+    }
     else {
-        stack binary;
+        Stack binary;
         int print = number;
         while (number > 0) {
             int temp = number % 2;
             binary.push(temp);
             number /= 2;
         }
-        binary.stack_export(print);
+        binary.stackExport(print);
     }
     cout << " (2)" << endl;
 }
-void to_octal(int number) {
-    if (number == 0)
+void toOctal(int number) {
+    if (number == 0) {
         cout << number << " = " << 0;
+    }
     else {
-        stack octal;
+        Stack octal;
         int print = number;
         while (number > 0) {
             int temp = number % 8;
             octal.push(temp);
             number /= 8;
         }
-        octal.stack_export(print);
+        octal.stackExport(print);
     }
     cout << " (8)" << endl;
 }
-void to_hexa(int number) {
-    if (number == 0)
+void toHexa(int number) {
+    if (number == 0) {
         cout << number << " = " << 0;
+    }
     else {
-        stack hexa;
+        Stack hexa;
         int print = number;
         while (number > 0) {
             int temp = number % 16;
             hexa.push(temp);
             number /= 16;
         }
-        hexa.stack_export(print);
+        hexa.stackExport(print);
     }
     cout << " (16)" << endl;
 }
 
-void stack::stack_export(int x) {
+void Stack::stackExport(int x) {
     cout << x << " = ";
-    for (int i = current_size; i >= 0; i--) {
-        if (arr[i] < 10)
+    for (int i = currentSize; i >= 0; i--) {
+        if (arr[i] < 10) {
             cout << arr[i];
-        else cout << char(arr[i] + 55);
+        }
+        else {
+            cout << char(arr[i] + 55);
+        }
     }
 }
-void stack::push(int add_element) {
-    current_size++;
-    arr[current_size] = add_element;
+void Stack::push(int addElement) {
+    currentSize++;
+    arr[currentSize] = addElement;
 }
