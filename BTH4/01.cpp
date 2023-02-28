db04fd3d5554f7f94b68a6b5db790b3e327b0f99
Hack Kiếm Sĩ <84692657+phanxuanquang@users.noreply.github.com>
2023-03-01 00:37:37 +0700

    Use ChatGPT to optimize code

--- BTH4/01.cpp
+++ BTH4/01.cpp
@@ -1,84 +1,91 @@
 #include <iostream>
 using namespace std;
-class time {
+
+class Time {
     int hour;
     int minute;
     int second;
+
 public:
-    time() {
-    }
-    time(int hour, int minute, int second) {
+    Time(int hour = 0, int minute = 0, int second = 0) {
         this->hour = hour;
         this->minute = minute;
         this->second = second;
     }
-    time operator+(time);
-    void import();
-    void print();
+
+    Time operator+(Time other_time);
+
+    void input();
+    void output();
 };
-int main()
-{
-    time a, b;
-    cout << "Thoi diem dau tien: " << endl;
-    a.import();
-    system("cls");
-    cout << "Thoi diem thu hai: " << endl;
-    b.import();
-    system("cls");
-    cout << "Thoi gian nhap vao: " << endl;
+
+int main() {
+    Time time1, time2;
+
+    cout << "Nhap thoi diem dau tien: " << endl;
+    time1.input();
+
+    cout << "Nhap thoi diem thu hai: " << endl;
+    time2.input();
+
+    Time time_total = time1 + time2;
+
     cout << "Thoi diem dau tien la: ";
-    a.print();
-    cout << "Thoi diem thu hai:     ";
-    b.print();
-    cout << endl;
-    time time_total = a + b;
-    cout << "Tong thoi gian la ";
-    time_total.print();
-    cout << endl;
-    system("pause");
+    time1.output();
+
+    cout << "Thoi diem thu hai la:  ";
+    time2.output();
+
+    cout << "Tong thoi gian la:     ";
+    time_total.output();
+
+    return 0;
 }
-time time::operator+(time x) {
-    time temp(0, 0, 0);
-    temp.second = second + x.second;
-    temp.minute = minute + x.minute;
-    temp.hour = hour + x.hour;
-    while (temp.second >= 60) {
-        temp.minute++;
-        temp.second -= 60;
-    }
-    while (temp.minute >= 60) {
-        temp.hour++;
-        temp.minute -= 60;
-    }
-    return temp;
+
+Time Time::operator+(Time otherTime) {
+    int totalSeconds = second + otherTime.second;
+    int carryMinutes = totalSeconds / 60;
+    totalSeconds %= 60;
+
+    int totalMinutes = minute + otherTime.minute + carryMinutes;
+    int carryHours = totalMinutes / 60;
+    totalMinutes %= 60;
+
+    int totalHours = hour + otherTime.hour + carryHours;
+
+    return Time(totalHours, totalMinutes, totalSeconds);
 }
-void time::import() {
+
+void Time::input() {
     cout << "Nhap gio: ";
     cin >> hour;
     while (hour < 0) {
-        cout << "Gio phai la so nguyen duong" << endl;
-        cout << " Nhap lai gio: ";
+        cout << "Gio phai la so nguyen duong." << endl;
+        cout << "Nhap lai gio: ";
         cin >> hour;
     }
+
     cout << "Nhap phut: ";
     cin >> minute;
     while (minute < 0 || minute >= 60) {
-        cout << "Phut phai nam trong khoang (0;60)" << endl;
-        cout << " Nhap lai phut: ";
+        cout << "Phut phai nam trong khoang (0;60)." << endl;
+        cout << "Nhap lai phut: ";
         cin >> minute;
     }
+
     cout << "Nhap giay: ";
     cin >> second;
     while (second < 0 || second >= 60) {
-        cout << "Giay phai nam trong khoang (0;60)" << endl;
-        cout << " Nhap lai giay: ";
+        cout << "Giay phai nam trong khoang (0;60)." << endl;
+        cout << "Nhap lai giay: ";
         cin >> second;
     }
 }
-void time::print() {
+void Time::output() {
     if (minute == 0 && second == 0)
         cout << hour << " gio " << endl;
     else if (second == 0)
         cout << hour << " gio " << minute << " phut " << endl;
-    else cout << hour << " gio " << minute << " phut " << second << " giay " << endl;
+    else
+        cout << hour << " gio " << minute << " phut " << second << " giay " << endl;
 }
