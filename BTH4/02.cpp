7ec5989ddb81ccd11cebe57bf1b84678766de377
Hack Kiếm Sĩ <84692657+phanxuanquang@users.noreply.github.com>
2023-03-31 14:30:43 +0700

    Update 02.cpp

--- BTH4/02.cpp
+++ BTH4/02.cpp
@@ -1,41 +1,44 @@
 #include <iostream>
 using namespace std;
 
-class stack {
-	int max_size, current_size;
-	int* arr = new int[max_size];
+class Stack {
+	int maxSize, currentSize;
+	int* arr = new int[maxSize];
 public:
-	stack(){
-		current_size = -1;
+	Stack() {
+		currentSize = -1;
 	}
-	void import_size();
-	void stack_export();
+	void importSize();
+	void output();
+	void deleteStack();
+
 	bool isEmpty();
 	bool isFull();
+
 	void push(int);
 	void pop();
-	int getTop();
-	void deleteStack();
+
+	int getValueAtTop();
 };
-void addElement_to(stack&);
-void call_menu();
-void choose(stack&);
+void addElementTo(Stack&);
+void showMenu();
+void handleTasksFor(Stack&);
 
 int main() {
-	stack Stack;
-	Stack.import_size();
-	call_menu();
-	choose(Stack);
+	Stack Stack;
+	Stack.importSize();
+	showMenu();
+	handleTasksFor(Stack);
 }
 
-void addElement_to(stack& x) {
-	int add_element;
+void addElementTo(Stack& stack) {
+	int element;
 	cout << "Phan tu muon them la: ";
-	cin >> add_element;
+	cin >> element;
 	system("cls");
-	x.push(add_element);
+	stack.push(element);
 }
-void call_menu() {
+void showMenu() {
 	cout << "_____________________________________________" << endl;
 	cout << "1. Push (them phan tu tren dau ngan xep)" << endl;
 	cout << "2. Pop (lay ra phan tu tren dau ngan xep" << endl;
@@ -44,98 +47,83 @@ void call_menu() {
 	cout << "5. Xoa ngan xep da tao" << endl;
 	cout << "_____________________________________________" << endl;
 }
-void choose(stack& x) {
+void handleTasksFor(Stack& stack) {
 	int choice;
 	cout << "Lua chon cua ban la: ";
 	cin >> choice;
+	system("cls");
 	switch (choice) {
 	case 1:
-		system("cls");
-		addElement_to(x);
-		call_menu();
-		choose(x);
+		addElementTo(stack);
 		break;
 	case 2:
-		system("cls");
-		x.pop();
-		call_menu();
-		choose(x);
+		stack.pop();
 		break;
 	case 3:
-		system("cls");
-		cout << "Phan tu tren dau ngan xep la " << x.getTop() << endl;
-		call_menu();
-		choose(x);
+		cout << "Phan tu tren dau ngan xep la " << stack.getValueAtTop() << endl;
 		break;
 	case 4:
-		system("cls");
-		x.stack_export();
-		call_menu();
-		choose(x);
+		stack.output();
 		break;
 	case 5:
-		system("cls");
-		x.deleteStack();
-		cout << "Da xoa ngan xep" << endl;
-		call_menu();
-		choose(x);
+		stack.deleteStack();
 		break;
 	default:
-		system("cls");
-		cout << "Lua chon khong hop le, hay nhap lai" << endl;
-		call_menu();
-		choose(x);
+		cout << "Lua chon khong hop le, hay nhap lai." << endl;
 		break;
 	}
+	showMenu();
+	handleTasksFor(stack);
 }
 
-void stack::import_size() {
+void Stack::importSize() {
 	cout << "Cho trong toi da cua ngan xep la ";
-	cin >> max_size;
+	cin >> maxSize;
 }
-void stack::stack_export() {
+void Stack::output() {
 	if (!isEmpty()) {
 		cout << "Ngan xep hien tai la: ";
-		for (int i = current_size; i >= 0; i--)
+		for (int i = currentSize; i >= 0; i--)
 			cout << arr[i] << " ";
 	}
 	else cout << "Ngan xep rong";
 	cout << endl;
 }
-bool stack::isEmpty() {
-	if (current_size == -1)
+bool Stack::isEmpty() {
+	if (currentSize == -1)
 		return true;
 	return false;
 }
-bool stack::isFull() {
-	if (current_size == max_size - 1)
+bool Stack::isFull() {
+	if (currentSize == maxSize - 1)
 		return true;
 	return false;
 }
-void stack::push(int add_element) {
+void Stack::push(int element) {
 	if (!isFull()) {
-		current_size++;
-		arr[current_size] = add_element;
-		cout << "Da them phan tu " << add_element << " vao ngan xep" << endl;
+		currentSize++;
+		arr[currentSize] = element;
+		cout << "Da them phan tu " << element << " vao ngan xep" << endl;
 	}
 	else cout << "Ngan xep da day" << endl;
 }
-void stack::pop() {
+void Stack::pop() {
 	if (!isEmpty()) {
 
-		cout << "Da lay phan tu " << arr[current_size] << " khoi ngan xep" << endl;
-		current_size--;
+		cout << "Da lay phan tu " << arr[currentSize] << " khoi ngan xep" << endl;
+		currentSize--;
 	}
 	else cout << "Ngan xep rong" << endl;
 }
-int stack::getTop() {
+int Stack::getValueAtTop() {
 	if (!isEmpty())
-		return arr[current_size];
+		return arr[currentSize];
 	else {
 		cout << "Ngan xep rong" << endl;
 		return -1;
 	}
 }
-void stack::deleteStack() {
-	current_size = -1;
+void Stack::deleteStack() {
+	currentSize = -1;
+	cout << "Da xoa ngan xep." << endl;
 }
