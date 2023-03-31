5f7c318944a7b5a551301b065b00370460e27ddb
Hack Kiếm Sĩ <84692657+phanxuanquang@users.noreply.github.com>
2023-03-31 14:45:05 +0700

    Update 01.cpp

--- BTH6/01.cpp
+++ BTH6/01.cpp
@@ -1,38 +1,39 @@
 #include <iostream>
 using namespace std;
 
-class complex {
-private:
+class ComplexNumber {
 	double real, imaginary;
+
 public:
+	friend istream& operator>>(istream& is, ComplexNumber& a);
+	friend ostream& operator<<(ostream& os, ComplexNumber& a);
 
-	friend istream& operator>>(istream& is, complex& a);
-	friend ostream& operator<<(ostream& os, complex& a);
+	ComplexNumber operator+(ComplexNumber a);
+	ComplexNumber operator-(ComplexNumber a);
+	ComplexNumber operator*(ComplexNumber a);
+	ComplexNumber operator/(ComplexNumber a);
 
-	complex operator+(complex a);
-	complex operator-(complex a);
-	complex operator*(complex a);
-	complex operator/(complex a);
-
-	bool operator==(complex a);
-	bool operator!=(complex a);
-	bool operator>(complex a);
-	bool operator>=(complex a);
-	bool operator<(complex a);
-	bool operator<=(complex a);
+	bool operator==(ComplexNumber a);
+	bool operator!=(ComplexNumber a);
+	bool operator>(ComplexNumber a);
+	bool operator>=(ComplexNumber a);
+	bool operator<(ComplexNumber a);
+	bool operator<=(ComplexNumber a);
 };
 
 int main() {
-	complex a, b, c;
+	ComplexNumber a, b, c;
 	bool result;
 	cout << "So thuc thu nhat: " << endl;
 	cin >> a;
 	cout << "So thuc thu hai: " << endl;
 	cin >> b;
 	system("cls");
+
 	cout << "a = " << a << endl;
 	cout << "b = " << b << endl;
 	cout << endl;
+
 	c = a + b;
 	cout << "a + b = " << c << endl;
 	c = a - b;
@@ -42,6 +43,7 @@ int main() {
 	c = a / b;
 	cout << "a : b = " << c << endl;
 	cout << endl;
+
 	if (a == b)
 		cout << a << " = " << b << endl;
 	if (a != b)
@@ -55,11 +57,12 @@ int main() {
 	if (a < b)
 		cout << a << " < " << b << endl;
 	cout << endl;
+
 	system("pause");
 }
 
 
-istream& operator>>(istream& is, complex& a) {
+istream& operator>>(istream& is, ComplexNumber& a) {
 	cout << " Phan thuc: ";
 	is >> a.real;
 	cout << " Phan ao: ";
@@ -67,7 +70,7 @@ istream& operator>>(istream& is, complex& a) {
 	return is;
 }
 
-ostream& operator<<(ostream& os, complex& a) {
+ostream& operator<<(ostream& os, ComplexNumber& a) {
 	if (a.imaginary > 0)
 		cout << a.real << " + " << a.imaginary << "i";
 	else if (a.imaginary == 0)
@@ -76,35 +79,35 @@ ostream& operator<<(ostream& os, complex& a) {
 	return os;
 }
 
-complex complex::operator+(complex a) {
-	complex result;
+ComplexNumber ComplexNumber::operator+(ComplexNumber a) {
+	ComplexNumber result;
 	result.real = real + a.real;
 	result.imaginary = imaginary + a.imaginary;
 	return result;
 }
 
-complex complex::operator-(complex a) {
-	complex result;
+ComplexNumber ComplexNumber::operator-(ComplexNumber a) {
+	ComplexNumber result;
 	result.real = real - a.real;
 	result.imaginary = imaginary - a.imaginary;
 	return result;
 }
 
-complex complex::operator*(complex a) {
-	complex result;
+ComplexNumber ComplexNumber::operator*(ComplexNumber a) {
+	ComplexNumber result;
 	result.real = real * a.real - imaginary * a.imaginary;
 	result.imaginary = real * a.imaginary + imaginary * a.real;
 	return result;
 }
 
-complex complex::operator/(complex a) {
-	complex result;
+ComplexNumber ComplexNumber::operator/(ComplexNumber a) {
+	ComplexNumber result;
 	result.real = (real * a.real + imaginary * a.imaginary) / sqrt(a.real * a.real + a.imaginary * a.imaginary);
 	result.imaginary = (real * a.imaginary - imaginary * a.real) / sqrt(a.real * a.real + a.imaginary * a.imaginary);
 	return result;
 }
 
-bool complex::operator==(complex a) {
+bool ComplexNumber::operator==(ComplexNumber a) {
 	double module = real * real + imaginary * imaginary;
 	double moduleA = a.real * a.real + a.imaginary * a.imaginary;
 	if (module == moduleA)
@@ -112,7 +115,7 @@ bool complex::operator==(complex a) {
 	return false;
 }
 
-bool complex::operator!=(complex a) {
+bool ComplexNumber::operator!=(ComplexNumber a) {
 	double module = real * real + imaginary * imaginary;
 	double moduleA = a.real * a.real + a.imaginary * a.imaginary;
 	if (module != moduleA)
@@ -120,7 +123,7 @@ bool complex::operator!=(complex a) {
 	return false;
 }
 
-bool complex::operator>(complex a) {
+bool ComplexNumber::operator>(ComplexNumber a) {
 	double module = real * real + imaginary * imaginary;
 	double moduleA = a.real * a.real + a.imaginary * a.imaginary;
 	if (module > moduleA)
@@ -128,7 +131,7 @@ bool complex::operator>(complex a) {
 	return false;
 }
 
-bool complex::operator>=(complex a) {
+bool ComplexNumber::operator>=(ComplexNumber a) {
 	double module = real * real + imaginary * imaginary;
 	double moduleA = a.real * a.real + a.imaginary * a.imaginary;
 	if (module >= moduleA)
@@ -136,7 +139,7 @@ bool complex::operator>=(complex a) {
 	return false;
 }
 
-bool complex::operator<(complex a) {
+bool ComplexNumber::operator<(ComplexNumber a) {
 	double module = real * real + imaginary * imaginary;
 	double moduleA = a.real * a.real + a.imaginary * a.imaginary;
 	if (module < moduleA)
@@ -144,7 +147,7 @@ bool complex::operator<(complex a) {
 	return false;
 }
 
-bool complex::operator<=(complex a) {
+bool ComplexNumber::operator<=(ComplexNumber a) {
 	double module = real * real + imaginary * imaginary;
 	double moduleA = a.real * a.real + a.imaginary * a.imaginary;
 	if (module <= moduleA)
